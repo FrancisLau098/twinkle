2f59ce24715948dde4eb22575cd82ffacd2d04f8
FrancisLau098 <35986375+FrancisLau098@users.noreply.github.com>
2018-10-27 13:39:36 +0800

    FIR

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+  chain: "https://node.cryptape.com",
+  privateKey:
+    "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+  contractAddress: "0x51C47AdE5adB368fF43633719B3657f8F20e395c"
+};
+module.exports = config;
